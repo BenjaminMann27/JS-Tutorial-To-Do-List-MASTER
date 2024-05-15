9bbe3a40ac3a14f73e1990324931bc0e8d8768c0
BenjaminMann27 <bmann09@icloud.com>
2024-05-15 13:37:56 -0400

    js 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 // CODE EXPLAINED channel
+const dateElement = document.getElementById('date');
+const clear = document.querySelector('.clear');
+const list = document.getElementById('list');
+const input = document.querySelector('input');
