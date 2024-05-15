2ad360fd9db3d26642bc7be5dc0a68d948288cef
BenjaminMann27 <bmann09@icloud.com>
2024-05-15 13:38:21 -0400

    js2

--- js/app.js
+++ js/app.js
@@ -3,3 +3,7 @@ const dateElement = document.getElementById('date');
 const clear = document.querySelector('.clear');
 const list = document.getElementById('list');
 const input = document.querySelector('input');
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
